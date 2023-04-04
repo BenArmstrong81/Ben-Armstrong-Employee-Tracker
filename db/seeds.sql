726702eb3ab33d2adee5c08e0e1fc4ea2f6323dc
Ben Armstrong <benarmstrong@live.com>
2023-04-04 21:29:07 +0930

    fixed schema issue

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
-----------Referring to Database that the Application will use:
+-- Referring to Database that the Application will use:
 USE employees_db;
 
-----------First Table to Store Data; "Department":
+-- First Table to Store Data; "Department":
 INSERT INTO department (department_name)
 VALUES ("Executive"),
        ("Treasuries"),
@@ -12,7 +12,7 @@ VALUES ("Executive"),
        ("Data, Analytics, Info Mgmt"),
        ("Information and Technology");
 
-----------Second Table to Store Data; "Role":
+-- Second Table to Store Data; "Role":
 INSERT INTO role (title, salary, department_id)
 VALUES ("Head Of", 250000, 1),
        ("Senior Leader", 180000, 2),
@@ -23,7 +23,7 @@ VALUES ("Head Of", 250000, 1),
        ("Processor", 75000, 7),
        ("Admin", 62000, 8);
 
-----------Third Table to Store Data; "Employees":
+-- Third Table to Store Data; "Employees":
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ("Richard", "Branson", 1, NULL),
        ("Michael", "Schumacher", 2, 1),
